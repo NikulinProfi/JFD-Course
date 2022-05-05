008aa6f65f5dab314be298f3a45783847d93bf81
Dmitriy <limob4125@yandex.ru>
2022-05-06 01:58:48 +0300

    hw5 done

---
+++ module1-lesson5/task2/index.js
@@ -0,0 +1,15 @@
+const myName = 'Дмитрий'
+const programmingLanguage = 'JavaScript'
+const courseCreatorName = 'Владилена Минина'
+const reasonText = 'хочу получать удовольствие от результатов своей работы'
+const numberOfMonth = '1'
+
+let myInfoText = `Всем привет! Меня зовут, ${myName}. Cейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}.
+
+Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал ${programmingLanguage} ${numberOfMonth} месяц. Я уверен, что пройду данный курс до конца!`
+console.log(myInfoText)
+console.log(myInfoText.replaceAll('JavaScript', 'JAVASCRIPT'))
+
+console.log('Длина строки myInfoText:', myInfoText.length)
+
+console.log(myInfoText[0] + myInfoText[myInfoText.length - 1])
