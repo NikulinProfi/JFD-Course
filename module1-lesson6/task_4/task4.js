83ac6e0aba907459050098fa3d0353fa8bba60d4
Dmitriy <limob4125@yandex.ru>
2022-05-08 17:27:54 +0300

    lesson6-task4-5 done

--- module1-lesson6/task_4/task4.js
+++ module1-lesson6/task_4/task4.js
@@ -1,34 +1,21 @@
-// for (let i = 0; i < 3; i += 1) {
-//   let newStudent = prompt('Введите имя нового студента!')
-//   if (newStudent) {
-//     newStudent = newStudent.trim()
-//     alert(`Добро пожаловать, ${newStudent}!`)
-//   }
-// }
-
-// do {
-
-// } while (условие)
+// Цикл while
 
 // let i = 0
-// do {
+// while (i < 3) {
 //   let newStudent = prompt('Введите имя нового студента!')
-//   if (newStudent) newStudent = newStudent.trim()
-//   alert(`Добро пожаловать, ${newStudent}!`)
-// } while (i < 3)
-
-// while (условие) {
-
-// }
-
-// let i = 0
-// while (i < 10) {
-//   console.log('i', i)
-//   i += 1
-// }
-
-// let newStudent = prompt('Введите имя нового студента!')
-// while ((newStudent < 3, (newStudent += 1))) {
 //   newStudent = newStudent.trim()
+//   console.log(newStudent)
+//   i++
 //   alert(`Добро пожаловать, ${newStudent}!`)
 // }
+
+// Цикл do - while
+
+let i = 0
+do {
+  let newStudent = prompt('Введите имя нового студента!')
+  newStudent = newStudent.trim()
+  console.log(newStudent)
+  i++
+  alert(`Добро пожаловать, ${newStudent}!`)
+} while (i < 3)
