176d5fc0a69cb463defc03a76d5d6ffb90c21b9c
Dmitriy <limob4125@yandex.ru>
2022-05-09 18:23:04 +0300

    lesson7-debug-task1-done

---
+++ module1-lesson7/task1/task1.js
@@ -0,0 +1,9 @@
+let health = Number(prompt('Введите число параметра "здоровье" для персонажа'))
+console.log('health', typeof health)
+if (health <= 0 || !health) {
+  console.log('health', health)
+  alert('Параметр "здоровье" должен быть больше нуля!')
+} else {
+  alert(`Параметр "здоровье" равен ${health}`)
+  console.log('health', health)
+}
