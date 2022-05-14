79838125d890c4497d0530a9bd6e5b853a4ac1b1
Dmitriy <limob4125@yandex.ru>
2022-05-14 11:26:51 +0300

    lesson8-all-tasks-done-upd

--- module1-lesson8/task1/task1.js
+++ module1-lesson8/task1/task1.js
@@ -1,24 +1,24 @@
 // Function Declaration
 const isName1 = getName1('Dima')
 function getName1(name) {
-  return name
+  return `Имя равно ${name}`
 }
-// const isName = getName1('Dima') // можно вызвать и до и после функции
-console.log(`Имя равно ${isName1}`)
+const isName = getName1('Dima') // можно вызвать и до и после функции
+console.log(isName1)
 
 // Function Expression
 
 function getName2(name) {
-  return name
+  return `Имя равно ${name}`
 }
 
 const isName2 = getName2('Oleg')
-console.log(`Имя равно ${isName2}`)
+console.log(isName2)
 
 // arrow Function Expression
 
 const getName3 = (name) => {
-  return name
+  return `Имя равно ${name}`
 }
 const isName3 = getName3('Denis')
-console.log(`Имя равно ${isName3}`)
+console.log(isName3)
--- module1-lesson8/task2/task2.js
+++ module1-lesson8/task2/task2.js
@@ -2,12 +2,14 @@ const getSumOfNumbers = function (number, type = 'odd') {
   let sum = 0
 
   for (let i = 0; i <= number; i++) {
+    const isEven = i % 2 === 0
+    const isOdd = !isEven
     if (type === 'odd') {
-      if (i % 2 > 0) {
+      if (isOdd) {
         sum += i
       }
     } else if (type === 'even') {
-      if (i % 2 === 0) {
+      if (isEven) {
         sum += i
       }
     } else if (type === '') {
