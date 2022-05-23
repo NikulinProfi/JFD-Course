56630a416ec7907138b49d38fd62278bd90ed0dc
Dmitriy <limob4125@yandex.ru>
2022-05-23 13:11:21 +0300

    lesson10-11-tasks: task2-done

--- module1-lesson11/task2/task2.js
+++ module1-lesson11/task2/task2.js
@@ -1,16 +1,19 @@
 function getDaysBeforeBirthday(nextBirthdayDate) {
-  nextBirthdayDate = {
-    day: 19,
-    month: 4,
-    year: 2023,
-  }
-  console.log(nextBirthdayDate)
-  dateNow = Date.now()
-  console.log(dateNow)
+  nextBirthdayDate = new Date(2023, 4, 19)
+  // console.log(nextBirthdayDate)
+  const dayNow = Date.now()
+  // console.log(dayNow)
+  difference = nextBirthdayDate - dayNow
+  // console.log(difference)
+  return difference
+}
+function convertMsToDays() {
+  const convertMsToDays = getDaysBeforeBirthday() / 1000 / 60 / 60 / 24
+  // console.log(convertMsToDays)
+  const round = Math.round(convertMsToDays)
+  // console.log(round)
+  return round
 }
 
-// function convertMsToDays() {
-//   Math.round(getDaysBeforeBirthday())
-// }
-
 getDaysBeforeBirthday()
+convertMsToDays()
